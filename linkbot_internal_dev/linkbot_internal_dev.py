3ac15071c123147a234e082e723bdc53bd8f8a73
David Ko <david.dk.ko@gmail.com>
2016-01-05 10:08:03 -0800

    ui displays...

--- linkbot_internal_dev/linkbot_internal_dev.py
+++ linkbot_internal_dev/linkbot_internal_dev.py
@@ -15,6 +15,11 @@ try:
 except:
     import preassembly
 
+try: 
+    from linkbot_internal_dev import tests 
+except:
+    import tests 
+
 #import linkbot_diagnostics as diagnostics
 from linkbot_diagnostics.LinkbotDiagnosticGui import initialize_tables
 from linkbot_diagnostics.LinkbotDiagnosticGui import LinkbotDiagnostic 
@@ -35,6 +40,9 @@ class StartQT4(QtGui.QMainWindow):
     diagnostics_finished = QtCore.pyqtSignal(int)
     diagnostics_error = QtCore.pyqtSignal(str, str) # (title, message)
 
+    tests = [ tests.Start, 
+            tests.SerialId, ]
+
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
@@ -42,158 +50,19 @@ class StartQT4(QtGui.QMainWindow):
 
         self.setWindowTitle('Linkbot Testing Suite ' + __version__)
 
-        self.connect_preassembly_buttons()
+        self._tests = iter(self.tests)
+        self._test = None
 
-        self.ui.pushButton_getid.clicked.connect(self.get_id)
+        self.display_next_test()
 
-        self.ui.pushButton_setid.setEnabled(False)
-        self.ui.pushButton_setid.clicked.connect(self.set_id)
-        self.ui.lineEdit_id.returnPressed.connect(self.set_id)
-        self.ui.lineEdit_id.textChanged.connect(self.id_text_modified)
-
-        self.ui.pushButton_postassembly_start.clicked.connect(
-                self.start_diagnostics)
-        self.diagnostics_finished.connect(self.end_diagnostics)
-        self.diagnostics_error.connect(self.message_box)
+    def display_next_test(self):
+        if self._test is not None:
+            self.ui.test_content_layout.removeWidget(self._test)
+        self._test = next(self._tests)
+        self.ui.test_content_layout.addWidget(self._test(self, linkbot=linkbot.Linkbot()))
 
     def message_box(self, title, message):
         QtGui.QMessageBox.information(self, title, message)
-
-    def preassembly_reset(self):
-        try:
-            self.ui.stackedWidget.setCurrentIndex(0)
-            self.ui.groupBox_serialId.setEnabled(True)
-            self.ui.groupBox_postassembly.setEnabled(True)
-            self.test.exit()
-            self.test = None
-        except Exception as e:
-            print('During preassembly reset:', e)
-
-    def connect_preassembly_buttons(self):
-        self.ui.pushButton_preassembly_reset.clicked.connect(self.preassembly_reset)
-        self.ui.pushButton_preassembly_start.clicked.connect(self.preassembly_start)
-        self.ui.pushButton_preassembly_next1.clicked.connect(self.preassembly_1)
-        self.ui.pushButton_preassembly_next2.clicked.connect(self.preassembly_2)
-        self.ui.pushButton_preassembly_next3.clicked.connect(self.preassembly_3)
-        self.ui.pushButton_preassembly_next4.clicked.connect(self.preassembly_encoder)
-        self.ui.pushButton_next_encoder.clicked.connect(
-                self.preassembly_7)
-        self.ui.pushButton_preassembly_next5.clicked.connect(self.preassembly_8)
-        self.ui.pushButton_preassembly_next6.clicked.connect(self.preassembly_9)
-        self.ui.pushButton_preassembly_finish.clicked.connect(self.preassembly_finish)
-
-    def preassembly_start(self):
-        try:
-            self.linkbot = linkbot.Linkbot()
-            self.ui.groupBox_serialId.setEnabled(False)
-            self.ui.groupBox_postassembly.setEnabled(False)
-            self.ui.stackedWidget.setCurrentIndex(1)
-            self.test = preassembly.TestLedRed(self, linkbot=self.linkbot)
-            self.test.enter()
-        except Exception as e:
-            self.message_box("Error", str(traceback.format_exc()))
-
-    def preassembly_1(self):
-        self.ui.stackedWidget.setCurrentIndex(2)
-        self.test.exit()
-        self.test = preassembly.TestLedGreen(self, linkbot=self.linkbot)
-        self.test.enter()
-
-    def preassembly_2(self):
-        self.ui.stackedWidget.setCurrentIndex(3)
-        self.test.exit()
-        self.test = preassembly.TestLedBlue(self, linkbot=self.linkbot)
-        self.test.enter()
-
-    def preassembly_3(self):
-        self.ui.stackedWidget.setCurrentIndex(4)
-        self.test.exit()
-        self.test = preassembly.TestButtonPower(self, linkbot=self.linkbot)
-        self.test.finished.connect(self.preassembly_4)
-        self.test.enter()
-
-    def preassembly_4(self):
-        self.ui.stackedWidget.setCurrentIndex(5)
-        self.test.exit()
-        self.test = preassembly.TestButtonA(self, linkbot=self.linkbot)
-        self.test.finished.connect(self.preassembly_5)
-        self.test.enter()
-
-    def preassembly_5(self):
-        self.ui.stackedWidget.setCurrentIndex(6)
-        self.test.exit()
-        self.test = preassembly.TestButtonB(self, linkbot=self.linkbot)
-        self.test.finished.connect(self.preassembly_6)
-        self.test.enter()
-
-    def preassembly_6(self):
-        self.ui.stackedWidget.setCurrentIndex(7)
-        self.test.exit()
-        self.test = preassembly.TestAccel(self, linkbot=self.linkbot)
-        self.test.finished.connect(self.preassembly_7)
-        self.test.x_changed.connect(self.ui.verticalSlider_x.setValue)
-        self.test.y_changed.connect(self.ui.verticalSlider_y.setValue)
-        self.test.z_changed.connect(self.ui.verticalSlider_z.setValue)
-        self.test.enter()
-
-    def preassembly_encoder(self):
-        self.ui.stackedWidget.setCurrentIndex(8)
-        self.test.exit()
-        self.test = preassembly.TestEncoders(self, linkbot=self.linkbot)
-        self.test.m1_changed.connect(self.ui.verticalSlider_1.setValue)
-        self.test.m2_changed.connect(self.ui.verticalSlider_2.setValue)
-        self.test.m3_changed.connect(self.ui.verticalSlider_3.setValue)
-        self.test.enter()
-
-    def preassembly_7(self):
-        self.ui.stackedWidget.setCurrentIndex(9)
-        self.test.exit()
-        self.test = preassembly.TestMotorCcw(self, linkbot=self.linkbot)
-        self.test.enter()
-
-    def preassembly_8(self):
-        self.ui.stackedWidget.setCurrentIndex(10)
-        self.test.exit()
-        self.test = preassembly.TestMotorCw(self, linkbot=self.linkbot)
-        self.test.enter()
-
-    def preassembly_9(self):
-        self.ui.stackedWidget.setCurrentIndex(11)
-        self.test.exit()
-        self.test = preassembly.TestBeep(self, linkbot=self.linkbot)
-        self.test.enter()
-        self.ui.pushButton_beep.clicked.connect(self.test.enter)
-
-    def preassembly_finish(self):
-        self.ui.stackedWidget.setCurrentIndex(0)
-        self.preassembly_reset()
-
-    def get_id(self):
-        try:
-            l = linkbot.Linkbot()
-            self.message_box("Serial ID", l.get_serial_id())
-        except:
-            self.message_box("Error", str(traceback.format_exc()))
-
-    def set_id(self):
-        if len(self.ui.lineEdit_id.text()) != 4:
-            self.message_box("Error", 
-                    "Serial ID must be 4 characters in length.")
-            return
-        try:
-            l = linkbot.Linkbot()
-            l._setSerialId(self.ui.lineEdit_id.text().upper())
-            l.set_buzzer_frequency(440)
-            time.sleep(0.4)
-            l.set_buzzer_frequency(0)
-        except:
-            self.message_box("Error", str(traceback.format_exc()))
-
-    def id_text_modified(self, text):
-        if len(text) == 4:
-            self.ui.pushButton_setid.setEnabled(True)
-        else:
-            self.ui.pushButton_setid.setEnabled(False)
 
     def start_diagnostics(self):
         self.ui.groupBox.setEnabled(False)
